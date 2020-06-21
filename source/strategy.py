a96a3d4259f9c0ea5be91806ae01514d133bb993
jacemica <jmica@uci.edu>
2020-06-21 12:04:49 -0700

    basic golden cross

--- source/strategy.py
+++ source/strategy.py
@@ -6,25 +6,19 @@ from config import *
 BASE_URL = "https://paper-api.alpaca.markets"
 STOCKS = ["INTC", "AMD", "NVDA", "IBM", "WDC", "AAPL", "FB", "MSFT", "AMZN", "NFLX", "TWTR", "CRM", "DBX", "WORK", "TWLO", "SQ", "PYPL", "CSCO", "ADBE", "SNE", "NTODY", "LYFT", "UBER", "TSLA", "WFC", "JPM", "BAC", "AXP", "MA", "V", "DIS", "TGT", "WMT", "NKE", "KO", "KHC", "SBUX", "GRUB"]
 
-
-def moving_average(api, stocks, days):
-    for stock in stocks:
-        barSum = 0
-        ma = api.get_barset(stock, "1D", days)
-        barset = ma[stock]
-        for bar in barset:
-            barSum += bar.c
+def moving_average(api, stock, days):
+    barSum = 0
+    ma = api.get_barset(stock, "1D", days)
+    barset = ma[stock]
+    for bar in barset:
+        barSum += bar.c
 
     return barSum/days
 
-api = tradeapi.REST(API_KEY, SEC_KEY, BASE_URL)
-ma_50 = moving_average(api, STOCKS, 50)
+def is_golden_cross(api, stock):
+    return moving_average(api, stock, 50) > moving_average(api, stock, 200)
 
-print(ma_50)
-# barsum = 0
-# aapl_barset = barset["AAPL"]
-# for bar in aapl_barset:
-#     barsum += bar.c
+if __name__ == "__main__":
+    api = tradeapi.REST(API_KEY, SEC_KEY, BASE_URL)
 
-# print(barsum/50)
-print("EOF")
+    print("EOF")
