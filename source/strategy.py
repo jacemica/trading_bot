e85cd58834d50e55cc1ff57e54549a52fc5d8de5
jacemica <jmica@uci.edu>
2020-06-21 12:23:42 -0700

    bollinger bands added

--- source/strategy.py
+++ source/strategy.py
@@ -1,5 +1,5 @@
 import alpaca_trade_api as tradeapi
-import requests, json
+import requests, json, statistics
 from pprint import pprint
 from config import * 
 
@@ -18,7 +18,23 @@ def moving_average(api, stock, days):
 def is_golden_cross(api, stock):
     return moving_average(api, stock, 50) > moving_average(api, stock, 200)
 
+def bollinger_bands(api, stock):
+    sma = moving_average(api, stock, 20)
+    std_dev = standard_deviation(api, stock)
+    UBB = sma + (2 * std_dev)
+    LBB = sma - (2 * std_dev)
+
+    return (LBB, UBB)
+
+def standard_deviation(api, stock):
+    close_list = []
+    ma = api.get_barset(stock, "1D", 20)
+    barset = ma[stock]
+    for bar in barset:
+        close_list.append(bar.c)
+
+    return statistics.pstdev(close_list)
 if __name__ == "__main__":
     api = tradeapi.REST(API_KEY, SEC_KEY, BASE_URL)
-
+    print(bollinger_bands(api, "MSFT"))
     print("EOF")
