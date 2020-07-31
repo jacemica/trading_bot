631841c5eab4396828afc389d01f42e47f74892e
jacemica <jmica@uci.edu>
2020-07-30 17:43:49 -0700

    removed cooldown

--- source/backtest/backtest.py
+++ source/backtest/backtest.py
@@ -2,7 +2,7 @@ import backtrader as bt
 import datetime
 from test_strategy import TestStrategy
 
-STOCK = 'INTC'
+STOCK = 'IBM'
 cerebro = bt.Cerebro()
 cerebro.broker.set_cash(5000)
 print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
@@ -12,9 +12,9 @@ data = bt.feeds.YahooFinanceCSVData(
     dataname='source/backtest/Historical_Data/{}.csv'.format(STOCK),
 
     # Do not pass values before this date
-    fromdate=datetime.datetime(2017, 10, 1),
+    fromdate=datetime.datetime(2019, 10, 1),
     # Do not pass values after this date
-    todate=datetime.datetime(2018, 9, 30),
+    todate=datetime.datetime(2020, 7, 14),
     reverse=False)
 
 cerebro.adddata(data)
